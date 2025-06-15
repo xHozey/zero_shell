10513294c683a1a697255fa391c841cd1a295f3f
xHozey <hamzasaliheddine@hotmail.com>
2025-06-15 17:30:25 +0100

    add: rm

--- src/commands/rm.rs
+++ src/commands/rm.rs
@@ -1,1 +1,38 @@
-pub fn rm(arg: String) {}
+use std::fs;
+use std::path::Path;
+pub fn rm(s: String) {
+    let mut flag = false;
+    let args: Vec<&str> = s.split_whitespace().collect();
+    if args.contains(&"-r") {
+        flag = true
+    } 
+    for file in args {
+        if file == "-r" {
+            continue;
+        }
+        let f = Path::new(file);
+        if f.exists() {
+            if flag {
+                if f.is_dir() {
+                    if let Err(err) = fs::remove_dir_all(f) {
+                        println!("{}", err.to_string())
+                    }
+                } else {
+                    if let Err(err) = fs::remove_file(f) {
+                        println!("{}", err.to_string());
+                    }
+                }
+            } else {
+                if f.is_dir() {
+                    println!("rm: cannot remove '{}': Is a directory", file)
+                } else {
+                    if let Err(err) = fs::remove_file(f) {
+                        println!("{}", err.to_string());
+                    }
+                }
+            }
+        } else {
+            println!("rm: cannot remove '{}': No such file or directory", file)
+        }
+    }
+}

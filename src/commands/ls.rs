2f118d3c19489d6dd03fc35f32740e114e1f5ea2
full-video-a11y <bakhouch2019@gmail.com>
2025-06-18 12:41:28 +0100

    fix: adding on ls

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -1,5 +1,8 @@
 use regex::Regex;
+use std::os::unix::fs::MetadataExt;
 use std::{fs};
+use std::fs::metadata;
+use std::os::unix::fs::PermissionsExt;
 use std::env;
 
 pub fn ls(s: String) {
@@ -8,8 +11,8 @@ pub fn ls(s: String) {
         ls_print(false);
     } else {
         let re = Regex::new(r"^(-[aFl]{1,3}(\s+|$)){1,3}$").unwrap();
-        if re.is_match(&s) {
-            for v in s.chars() {
+        if re.is_match(&s.trim()) {
+            for v in s.trim().chars() {
                 if v == 'a' {
                     ls_print(true);
                 }
@@ -24,7 +27,12 @@ fn ls_print(a:bool) {
                 let paths = fs::read_dir(&path).unwrap();
                 let mut arr = Vec::new();
                 for path_result in paths {
+                    // let meta = metadata(path_result.unwrap().path());
+                    //  let perm = meta.unwrap().permissions();
+                    // let perm_str : String= format!("{:?}", perm);
+                    // println!("{:?}", &perm_str[46..perm_str.len()-5]);
                     let path = path_result.unwrap().file_name().into_string().unwrap();
+
                     arr.push(path);
                 }
                 arr.sort();

658e6e54f68369a63c6ba348178ca89e9a7e6242
sbeytour <beytour.safae@gmail.com>
2025-07-16 17:38:52 +0100

    handle date and time

--- src/commands/ls/get_info.rs
+++ src/commands/ls/get_info.rs
@@ -1,28 +1,36 @@
+use chrono::{DateTime, Duration, Local};
 use std::{
     fs::{metadata, Metadata},
     os::unix::fs::MetadataExt,
     path::Path,
 };
 use users::{get_group_by_gid, get_user_by_uid};
-pub fn get_detailed_info(path: &Path) -> Result<String, ()> {
+pub fn get_detailed_info(path: &Path) -> Result<Vec<String>, ()> {
+    let mut details = Vec::new();
     let metadata = match metadata(path) {
         Ok(metadata) => metadata,
         Err(_) => return Err(()),
     };
 
     let permission = get_permissions(&metadata);
-    let owner = get_owner(&metadata);
-    let groupe = get_groupe(&metadata);
     let hard_link = metadata.nlink().to_string();
+    let owner = get_owner(&metadata);
+    let groupe = get_group(&metadata);
     let file_size = metadata.len().to_string();
+    let last_modif = match get_date_time(&metadata) {
+        Ok(date) => date,
+        Err(_) => return Err(()),
+    };
     let file_name = match path.file_name() {
         Some(name) => name.to_string_lossy().to_string(),
         None => String::from(""),
     };
-    Ok(format!(
-        " {}   {}    {}   {}    {}     {}",
-        permission, hard_link, owner, groupe, file_name, file_size
-    ))
+
+    details.push(format!(
+        " {}   {}    {}   {}    {}     {}   {}",
+        permission, hard_link, owner, groupe, file_size, last_modif, file_name
+    ));
+    Ok(details)
 }
 
 fn get_permissions(metadata: &Metadata) -> String {
@@ -32,22 +40,37 @@ fn get_permissions(metadata: &Metadata) -> String {
     permission.to_string()
 }
 
-pub fn totale_files() {}
-
 fn get_owner(metadata: &Metadata) -> String {
     let uid = metadata.uid();
 
     match get_user_by_uid(uid) {
         Some(user) => user.name().to_string_lossy().to_string(),
-        None => String::from(""),
+        None => String::from("{uid}"),
     }
 }
 
-fn get_groupe(metadata: &Metadata) -> String {
+fn get_group(metadata: &Metadata) -> String {
     let gid = metadata.gid();
 
     match get_group_by_gid(gid) {
         Some(groupe) => groupe.name().to_string_lossy().to_string(),
-        None => String::from(""),
+        None => String::from("{gid}"),
+    }
+}
+
+fn get_date_time(metadata: &Metadata) -> Result<String, ()> {
+    let syst_time = match metadata.modified() {
+        Ok(time) => time,
+        Err(_) => return Err(()),
+    };
+    let mut date: DateTime<Local> = syst_time.into();
+    date += Duration::seconds(3600);
+
+    let now = Local::now();
+    let six_month = now - Duration::seconds(6 * 30 * 24 * 60 * 60);
+    if date > six_month {
+        Ok(date.format("%b  %d  %H:%M").to_string())
+    } else {
+        Ok(date.format("%b  %d  %Y").to_string())
     }
 }
--- src/commands/ls/handlers.rs
+++ src/commands/ls/handlers.rs
@@ -13,7 +13,7 @@ pub fn handle_files(files: &Vec<String>, flags: &Flags) -> Result<Vec<Vec<String
                 Ok(info) => info,
                 Err(_) => return Err(()),
             };
-            infos.push(vec![detailed_info]);
+            infos.push(detailed_info);
         } else {
             infos.push(vec![file.to_string()]);
         }
@@ -27,42 +27,44 @@ pub fn handle_dir(dirs: &Vec<String>, flags: &Flags) -> Result<Vec<Vec<String>>,
     for dir in dirs {
         let dir_path = Path::new(dir);
 
-        if flags.a {
-            let hiden_files = match get_hidden_files(&dir_path, &flags) {
+        let all_files = if flags.a {
+            match get_hidden_files(&dir_path, &flags) {
                 Ok(files) => files,
                 Err(()) => return Err(()),
-            };
-            infos.push(hiden_files);
-        }
-
-        let all_files = match get_files(&dir_path, &flags) {
-            Ok(files) => files,
-            Err(()) => return Err(()),
+            }
+        } else {
+            match get_files(&dir_path, &flags) {
+                Ok(files) => files,
+                Err(()) => return Err(()),
+            }
         };
-        infos.push(all_files);
+
+        infos = all_files
     }
+
     println!("infoos {:?}", &infos);
     Ok(infos)
 }
 
-pub fn get_hidden_files(dir_path: &Path, flags: &Flags) -> Result<Vec<String>, ()> {
+pub fn get_hidden_files(dir_path: &Path, flags: &Flags) -> Result<Vec<Vec<String>>, ()> {
     let mut entries = Vec::new();
 
     match read_dir(dir_path) {
         Ok(dir_entries) => {
-            for entry in dir_entries {
-                if let Ok(dir_entry) = entry {
-                    if let Some(file_name) = dir_entry.file_name().to_str() {
-                        println!("{:?}", &dir_entry);
-                        if flags.l {
-                            let details = match get_detailed_info(&dir_entry.path()) {
-                                Ok(info) => info,
-                                Err(_) => return Err(()),
-                            };
-                            entries.push(details);
-                        } else {
-                            entries.push(file_name.to_string());
-                        }
+            let mut direntries: Vec<_> = dir_entries.filter_map(|entry| entry.ok()).collect();
+            direntries.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
+
+            for dir_entry in direntries {
+                if let Some(file_name) = dir_entry.file_name().to_str() {
+                    println!("{:?}", &dir_entry);
+                    if flags.l {
+                        let details = match get_detailed_info(&dir_entry.path()) {
+                            Ok(info) => info,
+                            Err(_) => return Err(()),
+                        };
+                        entries.push(details);
+                    } else {
+                        entries.push(vec![file_name.to_string()]);
                     }
                 }
             }
@@ -74,25 +76,26 @@ pub fn get_hidden_files(dir_path: &Path, flags: &Flags) -> Result<Vec<String>, (
     Ok(entries)
 }
 
-pub fn get_files(dir_path: &Path, flags: &Flags) -> Result<Vec<String>, ()> {
+pub fn get_files(dir_path: &Path, flags: &Flags) -> Result<Vec<Vec<String>>, ()> {
     let mut entries = Vec::new();
     match read_dir(dir_path) {
         Ok(dir_entries) => {
-            for entry in dir_entries {
-                dbg!(&entry);
-                if let Ok(dir_entry) = entry {
-                    if let Some(file_name) = dir_entry.file_name().to_str() {
-                        if !file_name.starts_with(".") {
-                            if flags.l {
-                                let details = match get_detailed_info(&dir_entry.path()) {
-                                    Ok(info) => info,
-                                    Err(_) => return Err(()),
-                                };
-                                entries.push(details);
-                                println!("{:?}", entries)
-                            } else {
-                                entries.push(file_name.to_string());
-                            }
+            let mut direntries: Vec<_> = dir_entries.filter_map(|entry| entry.ok()).collect();
+            direntries.sort_by(|a, b| a.file_name().cmp(&b.file_name()));
+
+            for dir_entry in direntries {
+                dbg!(&dir_entry);
+                if let Some(file_name) = dir_entry.file_name().to_str() {
+                    if !file_name.starts_with(".") {
+                        if flags.l {
+                            let details = match get_detailed_info(&dir_entry.path()) {
+                                Ok(info) => info,
+                                Err(_) => return Err(()),
+                            };
+                            entries.push(details);
+                            println!("{:?}", entries)
+                        } else {
+                            entries.push(vec![file_name.to_string()]);
                         }
                     }
                 }
@@ -100,7 +103,6 @@ pub fn get_files(dir_path: &Path, flags: &Flags) -> Result<Vec<String>, ()> {
         }
         Err(_) => return Err(()),
     };
-
     println!("final {:?}", &entries);
     Ok(entries)
 }

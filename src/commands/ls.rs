858267fd9f052ba406ec9e9b622d96deb9ea76b6
mbakhouc <bakhouch2019@gmail.com>
2025-07-16 17:35:20 +0100

    deleting comments

--- src/commands/ls.rs
+++ src/commands/ls.rs
@@ -5,19 +5,6 @@ use std::env;
 pub fn ls(s: String) {
     let res = ls_parse(s);
     println!("{:?}", res);
-    // let args: Vec<&str> = s.split_whitespace().collect();
-    // if args.len() == 0 {
-    //     ls_print(false);
-    // } else {
-    //     let re = Regex::new(r"^(-[aFl]{1,3}(\s+|$)){1,3}$").unwrap();
-    //     if re.is_match(&s.trim()) {
-    //         for v in s.trim().chars() {
-    //             if v == 'a' {
-    //                 ls_print(true);
-    //             }
-    //         }
-    //     }
-    // }
 }
 
 
@@ -56,31 +43,3 @@ fn ls_parse(usage: String) -> Result<(Vec<char>, Vec<String>), String> {
     Ok((user_commands, user_file_or_dir))
 
 }
-
-// fn ls_print(a:bool) {
-//     match env::current_dir() {
-//             Ok(path) => {
-
-//                 let paths = fs::read_dir(&path).unwrap();
-//                 let mut arr = Vec::new();
-//                 for path_result in paths {
-//                     // let meta = metadata(path_result.unwrap().path());
-//                     //  let perm = meta.unwrap().permissions();
-//                     // let perm_str : String= format!("{:?}", perm);
-//                     // println!("{:?}", &perm_str[46..perm_str.len()-5]);
-//                     let path = path_result.unwrap().file_name().into_string().unwrap();
-
-//                     arr.push(path);
-//                 }
-//                 arr.sort();
-//                 for file in arr {
-//                     if file.chars().nth(0) == Some('.') && !a {
-//                         continue;
-//                     }
-//                     print!("{}", file);
-//                 }
-//             },
-//             Err(err) => eprintln!("ls: {}", err.to_string().to_ascii_lowercase()),
-//         }
-// }
-

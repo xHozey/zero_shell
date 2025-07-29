bc931f439012926628e86d30e2413fbc42a5b2e4
xHozey <hamzasaliheddine@hotmail.com>
2025-07-29 16:58:20 +0100

    fix: mv cannot swap files content

--- src/commands/mv.rs
+++ src/commands/mv.rs
@@ -10,7 +10,38 @@ pub fn mv(args: Vec<String>) {
         eprintln!("mv: missing destination file operand after '{}'", args[0]);
         return;
     }
+
+    if args.len() == 2 {
+        let src = Path::new(&args[0]);
+        let dst = Path::new(&args[1]);
+
+        let tmp = dst.with_extension("tmp_swap");
+
+        if let Err(err) = rename(dst, &tmp) {
+            eprintln!("mv: {}", err);
+            return;
+        }
+        if let Err(err) = rename(src, dst) {
+            eprintln!("mv: {}", err);
+            return;
+        }
+        if let Err(err) = rename(&tmp, src) {
+            eprintln!("mv: {}", err);
+            return;
+        }
+        return;
+    }
+
     let dis = Path::new(&args[args.len() - 1]);
+    if !dis.is_dir() {
+        eprintln!(
+            "mv: target '{}' is not a directory",
+            dis.file_name()
+                .and_then(|f| f.to_str())
+                .unwrap_or("<invalid utf8>")
+        );
+        return;
+    }
     for i in 0..args.len() - 1 {
         if let Err(err) = rename(&args[i], dis.join(Path::new(&args[i]))) {
             eprintln!("mv: {}", err.to_string().to_ascii_lowercase())
